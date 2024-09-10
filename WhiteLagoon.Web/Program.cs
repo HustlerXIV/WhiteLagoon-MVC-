0950051e889239edf3b5bf2f21e35e59cdaaa537
TangNapat <napat.jaiyim@hotmail.com>
2024-09-10 21:43:02 +0700

    [UPDATES] Created VillaReposity and IVillaRepository

--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using WhiteLagood.Application.Common.Interfaces;
 using WhiteLagoon.Infrastructure.Data;
+using WhiteLagoon.Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 
 var app = builder.Build();
 
